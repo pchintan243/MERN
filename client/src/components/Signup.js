af4f8a8ad97cf9d43eabd6d212a1e6121a5e143b
Chintan Patel <patelchintan843@gmail.com>
2023-02-24 18:58:56 +0530

    v23 (registraion page)

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -2,7 +2,34 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <>
+      <div className="mainn">
+        <div className="main">
+          <input type="checkbox" id="chk" aria-hidden="true" />
+
+          <div className="signup">
+            <form>
+              <label htmlFor="chk" aria-hidden="true">Sign up</label>
+              <input type="text" name="username" placeholder="Name" required />
+              <input type="email" name="email" placeholder="Email" required />
+              <input type="text" name="phone" placeholder="Phone No." required />
+              <input type="password" name="password" placeholder="Password" autoComplete='off' required />
+              <input type="password" name="cpassword" placeholder="Confirm Password" autoComplete='off' required />
+              <button className='signup-btn'>Sign up</button>
+            </form>
+          </div>
+
+          <div className="login">
+            <form>
+              <label htmlFor="chk" aria-hidden="true">Login</label>
+              <input type="email" name="email" placeholder="Email" required />
+              <input type="password" name="password" placeholder="Password" autoComplete='off' required />
+              <button className='signup-btn'>Login</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
