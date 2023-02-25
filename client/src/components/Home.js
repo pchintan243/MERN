7798abc954b52b7babc4e83b6e5e335ad64c75b0
Chintan Patel <patelchintan843@gmail.com>
2023-02-25 15:41:39 +0530

    v26 (home page)

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <>
+            <div style={{ height: "85vh" }} className='d-flex flex-column justify-content-center align-items-center fw-bold h4'>
+                <p className='m-0'>
+                    WELCOME
+                </p>
+                <p className='m-0'>
+                    We are the MERN Developer
+                </p>
+            </div>
+        </>
     )
 }
 
