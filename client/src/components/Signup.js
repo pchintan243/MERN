b8d2c7d0ee5a824df8feb9bfb89c6c48b51ce8c1
Chintan Patel <patelchintan843@gmail.com>
2023-02-26 16:10:53 +0530

    v30 (store the user data using useState)

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignUp = () => {
+
+  // It takes user data field
+  // Mention all the field in the usestate hook
+  const [user, setUser] = useState({
+    name: "", email: "", phone: "", password: "", cpassword: ""
+  });
+
+  // To get the user data
+  // Also you can see on inspect element after components
+  let name, value;
+  const handleInputs = (e) => {
+    console.log(e);
+    name = e.target.name;
+    value = e.target.value;
+    setUser({ ...user, [name]: value })
+  }
+
   return (
     <>
       <div className="mainn">
@@ -10,11 +27,11 @@ const SignUp = () => {
           <div className="signup">
             <form>
               <label htmlFor="chk" aria-hidden="true">Sign up</label>
-              <input type="text" name="username" placeholder="Name" required />
-              <input type="email" name="email" placeholder="Email" required />
-              <input type="text" name="phone" placeholder="Phone No." required />
-              <input type="password" name="password" placeholder="Password" autoComplete='off' required />
-              <input type="password" name="cpassword" placeholder="Confirm Password" autoComplete='off' required />
+              <input type="text" name="name" placeholder="Name" value={user.name} onChange={handleInputs} required />
+              <input type="email" name="email" placeholder="Email" value={user.email} onChange={handleInputs} required />
+              <input type="text" name="phone" placeholder="Phone No." value={user.phone} onChange={handleInputs} required />
+              <input type="password" name="password" placeholder="Password" autoComplete='off' value={user.password} onChange={handleInputs} required />
+              <input type="password" name="cpassword" placeholder="Confirm Password" autoComplete='off' value={user.cpassword} onChange={handleInputs} required />
               <button className='signup-btn'>Sign up</button>
             </form>
           </div>
