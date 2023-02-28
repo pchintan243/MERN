d055f702b41cc4419463fd0705a60b68ccce3e4f
Chintan Patel <patelchintan843@gmail.com>
2023-02-28 19:36:14 +0530

    v39 (home page: show username in home page)

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+
+    const [userName, setUserName] = useState('');
+    // To see if user is login or not
+    const [show, setShow] = useState(false);
+
+    const userHomePage = async () => {
+        try {
+            const res = await fetch("/getdata", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            const data = await res.json();
+            console.log(data);
+            // To get the data
+            setUserName(data.name);
+            // If you will get the data means user was login so we show value changing to true
+            setShow(true);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        userHomePage();
+    }, [])
+
     return (
         <>
             <div style={{ height: "85vh" }} className='d-flex flex-column justify-content-center align-items-center fw-bold h4'>
                 <p className='m-0'>
                     WELCOME
                 </p>
-                <p className='m-0'>
-                    We are the MERN Developer
-                </p>
+                <h1 style={{ fontSize: "5rem" }}>{userName}</h1>
+                {/* User login then it shows different content */}
+                <h2>{show ? 'We are the MERN developer' : 'Happy to see you that'}</h2>
             </div>
         </>
     )
